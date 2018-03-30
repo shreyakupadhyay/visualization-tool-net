1561c7002cdc3cfcee1f376feda7a4fcac5ba809
shreyakupadhyay <shreyakupadhyay07@gmail.com>
2018-03-30 14:00:48 +0530

    setting up redux-dev-tools
    setting redux-dev-tools to debug easily by adding enchancer in the store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-export default function configureStore(initialState) {
+
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f 
+)
+
+
+export default function configureStore() {
     return createStore(
         rootReducer,
-        initialState,
-        applyMiddleware(thunk)
+        enhancers,
+        applyMiddleware(thunk),
     );
 }
